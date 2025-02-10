bdec0e8fd3d4d4717e2feaa256c29df2232e13d1
Prabhat Maurya <prabhat@e.email>
2025-02-10 14:34:00 +0530

    feat: Update app version to 1.0-beta
    This commit updates the app's version information in `build.gradle.kts` file:
    - Increments `versionCode` from 6 to 7.
    - Updates `versionName` from "1.0-alpha5" to "1.0-beta".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "dev.bugstitch.titanote"
         minSdk = 21
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.0-alpha5"
+        versionCode = 7
+        versionName = "1.0-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
